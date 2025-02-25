3b835b3edcbe9f3842d897e86495ebcbd24e3af7
metadreamm <soxegor@gmail.com>
2025-02-25 18:10:12 -0500

    added like and dislike buttons

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,30 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+        likes: 0,
+        dislikes: 0
+    };
+    }
     }
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className='content-rating'>
+        <p>
+        Did you know? Honey never spoils. Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still perfectly edible!
+        </p>
+
+        <div className='rating-buttons'>
+            <button className='like-button'>
+                Like ({this.state.likes})
+            </button>
+            <button className='dislike-button'>
+                Dislike ({this.state.dislikes})
+            </button>
+        </div>
+     </div>
      </>
     );
   }
